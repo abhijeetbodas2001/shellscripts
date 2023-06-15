e294c043410929ade3d93bdb3bb6f608ab428e57
Abhijeet Prasad Bodas <abhijeetbodas2001@gmail.com>
2023-06-15 18:14:03 +0530

    simplify backup script

--- backup_to_hdd.sh
+++ backup_to_hdd.sh
@@ -1,39 +1,12 @@
-#!/usr/bin/env bash
-
-set -o errexit; set -o nounset; set -o pipefail;
-if [[ "${TRACE-0}" == "1" ]]; then
-    set -o xtrace
-fi
-
-# Backs up data from SSD to Hard Drive
-# Note to self: Also make sure that everything in ~/code is
-# committed and pushed to GitHub
-
-# Paths
-HDDpath="/mnt/z"
-CmyDataPath="/mnt/c/myData"
-
-# Dirs with many changes (which give long outputs on running
-# the script) kept at the end of the list
-for dir in\
-    "AsusScreenshots"\
-    "AsusSoundRecorder"\
-    "Music"\
-    "Pictures"\
-    "Public"\
-    "Screenshots"\
-    "Templates"\
-    "Videos"\
-    "Webcam"\
-    "WhatsApp"\
-    "academic"\
-    "archive"\
-    "games"\
-    "misc"\
-    "notes"
-do
-    printf "\n\n"
-    echo "Starting "$dir" ----------------------------------------"
-    rsync -a -v --delete $CmyDataPath"/"$dir"/" $HDDpath"/"$dir"/"
-done
-
+# --delete -> delete files in dest which are not in source
+# --exclude="/.*/" -> exclude all dot-directories in home folder (like ~/.mozilla, ~/.vscode etc)
+# --include=".ssh/" -> exclude the above, other than the ~/.ssh
+# Other excludes -> exclude node_modules etc anywhere they appear in the file-tree
+rsync --archive --verbose --delete \
+--include=".ssh/" \
+--exclude="/.*/" \
+--exclude="node_modules/" \
+--exclude=".pnpm-store/" \
+--exclude="rubygems/" \
+--exclude=".cache/" \
+~/ /mnt/HDD/home_apb
